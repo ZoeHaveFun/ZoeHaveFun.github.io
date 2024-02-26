4a270a4d4729468fcf31ea67905afd1bafcba87f
ZoeHaveFun <unique84060327@gmail.com>
2024-02-26 18:31:02 +0800

    feat: change file structure

---
+++ src/components/sections.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from "react";
+
+export const Hero: FC = () => {
+  return(
+    <section>
+      <div>
+        <h1>Hello, I am Zoe </h1>
+      </div>
+    </section>
+  )
+};
