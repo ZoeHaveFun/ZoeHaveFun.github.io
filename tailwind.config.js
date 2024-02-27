d5eb66dcded71617e98e9d16b6587108cc902fbc
ZoeHaveFun <unique84060327@gmail.com>
2024-02-27 21:59:36 +0800

    style: theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      padding: {
+        '8%': '8%',
+      },
       colors: {
-        
+        "main-beige": "#E5E2D6",
+        "main-black": "#3E3E3C",
+        "main-violet": "#C69FB8",
+        "main-green": "#9FB378",
+        "violet-ash": "#B39CA3",
+        "violet-dark": "#715E6F",
+
       }
     },
+    clipPath: {
+      "polygonA": "polygon(100% 100%, 100% 0, 0 0)",
+      "polygonB": "polygon(0 0, 100% 0, 25% 100%)",
+      "polygonC": "polygon(100% 100%, 100% 0, 0 0)",
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-clip-path'),
+  ],
 }
 
