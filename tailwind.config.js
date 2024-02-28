1774b1a0d4ffdeb19daf6aed74bc692736761c03
ZoeHaveFun <unique84060327@gmail.com>
2024-02-28 14:38:35 +0800

    style: add green-light color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,13 @@ module.exports = {
         "main-green": "#9FB378",
         "violet-ash": "#B39CA3",
         "violet-dark": "#715E6F",
-
+        "green-light": "#D3CE9D",
       }
     },
     clipPath: {
       "polygonA": "polygon(100% 100%, 100% 0, 0 0)",
       "polygonB": "polygon(0 0, 100% 0, 25% 100%)",
-      "polygonC": "polygon(100% 100%, 100% 0, 0 0)",
+      "polygonC": "polygon(0 0, 100% 0, 0 100%)",
     },
   },
   plugins: [
