5f87c35f45484cfa6cd9d2a176bd46ccf1a1ce20
ZoeHaveFun <unique84060327@gmail.com>
2024-03-01 16:21:31 +0800

    feat: add website icon component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -48,7 +48,7 @@ export const icons = [
   },
   {
     name: "Firebase",
-  },
+  }
 ];
 
 export const IconsSVG = (name: string) => {
@@ -86,6 +86,8 @@ export const IconsSVG = (name: string) => {
       return <FigmaIcon className={iconsStyle} />;
     case "Firebase":
       return <FirebaseIcon className={iconsStyle} />;
+    case "Website": 
+      return <WebsiteIcon className={iconsStyle} />;
     default:
       return <QuestionIcon className={iconsStyle} />;
   }
@@ -220,6 +222,12 @@ export const FirebaseIcon: FC<SVGProps<SVGSVGElement>> = ({ ...props }) => (
     <path d="M93.19 329.38l47.45-304.07c1.64-10.37 15.55-12.82 20.46-3.55l51 95.45zM432 400l-46.74-276.79a11 11 0 00-18.54-6L80 400l159.36 91.91a33.18 33.18 0 0031.91 0zM302.36 158.93l-36.54-69.54a10.86 10.86 0 00-19.36 0L85.83 375.74z" />
   </svg>
 );
+
+export const WebsiteIcon: FC<SVGProps<SVGSVGElement>> = ({ ...props}) => (
+  <svg {...props} viewBox="0 0 24 24">
+    <path d="M16.36 14c.08-.66.14-1.32.14-2 0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2m-5.15 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95a8.03 8.03 0 01-4.33 3.56M14.34 14H9.66c-.1-.66-.16-1.32-.16-2 0-.68.06-1.35.16-2h4.68c.09.65.16 1.32.16 2 0 .68-.07 1.34-.16 2M12 19.96c-.83-1.2-1.5-2.53-1.91-3.96h3.82c-.41 1.43-1.08 2.76-1.91 3.96M8 8H5.08A7.923 7.923 0 019.4 4.44C8.8 5.55 8.35 6.75 8 8m-2.92 8H8c.35 1.25.8 2.45 1.4 3.56A8.008 8.008 0 015.08 16m-.82-2C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2 0 .68.06 1.34.14 2M12 4.03c.83 1.2 1.5 2.54 1.91 3.97h-3.82c.41-1.43 1.08-2.77 1.91-3.97M18.92 8h-2.95a15.65 15.65 0 00-1.38-3.56c1.84.63 3.37 1.9 4.33 3.56M12 2C6.47 2 2 6.5 2 12a10 10 0 0010 10 10 10 0 0010-10A10 10 0 0012 2z" />
+  </svg>
+)
 
 export const QuestionIcon: FC<SVGProps<SVGSVGElement>> = ({ ...props }) => (
   <svg {...props} viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg">
